ef479ded95decacc251754bd02027bc784d7a4e8
SupaatWisetsud <50663941+SupaatWisetsud@users.noreply.github.com>
2021-03-08 14:24:58 +0700

    [edit] end resolve marge

--- src/app/chapter2/preface/preface.component.ts
+++ src/app/chapter2/preface/preface.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-preface',
   templateUrl: './preface.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./preface.component.scss','../chapter2.component.scss']
-=======
   styleUrls: ['./preface.component.scss', '../chapter2.component.scss']
->>>>>>> be8d02a0869731eb871a444265ee1c060c168bb9
 })
 export class PrefaceComponent implements OnInit {
 
